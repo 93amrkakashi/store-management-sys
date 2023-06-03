e69d40de00ce516c4dafbf65ad1a6f69c51c3ccf
93amrkakashi <93amr.kakashi@gmail.com>
2023-06-02 22:57:38 -0700

    responsive

--- client/src/components/ProductsTable.jsx
+++ client/src/components/ProductsTable.jsx
@@ -10,57 +10,58 @@ const ProductsTable = ({
   const { user } = useAuthContext();
 
   return (
-    <div className=" min-w-full min-h-full ">
-      <table className="min-w-full min-h-full bg-gray-800 text-white text-left ">
-        <thead>
-          <tr>
-            <th className="px-4 py-2 border-b border-l border-t w-5/12	 ">
-              Name
-            </th>
-            <th className="px-4 py-2 border-b border-l border-t w-1/12	">
-              Quantity
-            </th>
-            <th className="px-4 py-2 border-b border-l border-t  w-1/12	">
-              Available
-            </th>
+    <div className="min-w-full min-h-full">
+  <table className="min-w-full min-h-full bg-gray-800 text-white text-left">
+    <thead>
+      <tr>
+        <th className="px-4 py-2 border-b border-l border-t w-5/12 sm:w-auto">
+          Name
+        </th>
+        <th className="px-4 py-2 border-b border-l border-t w-1/12 sm:w-auto">
+          Quantity
+        </th>
+        <th className="px-4 py-2 border-b border-l border-t w-1/12 sm:w-auto">
+          Available
+        </th>
 
-            {user.owner && (
-              <>
-                <th className="px-4 py-2 border-b border-l border-t  w-1/12	">
-                  IN
-                </th>
-                <th className="px-4 py-2 border-b border-l border-t w-1/12	 ">
-                  OUT
-                </th>
-                <th className="px-4 py-2 border-b border-l border-t  w-1/12	">
-                  Delete
-                </th>
-                <th className="px-4 py-2 border-b border-l border-t  w-1/12	">
-                  Apply
-                </th>
-              </>
-            )}
-          </tr>
-        </thead>
-        {!filterdProducts
-          ? products?.map((product) => (
-              <TAbleActions
-                key={product._id}
-                product={product}
-                fetchProducts={fetchProducts}
-                setFelterdProducts={setFelterdProducts}
-              />
-            ))
-          : filterdProducts?.map((product) => (
-              <TAbleActions
-                key={product._id}
-                product={product}
-                fetchProducts={fetchProducts}
-                setFelterdProducts={setFelterdProducts}
-              />
-            ))}
-      </table>
-    </div>
+        {user.owner && (
+          <>
+            <th className="px-4 py-2 border-b border-l border-t w-1/12 sm:w-auto">
+              IN
+            </th>
+            <th className="px-4 py-2 border-b border-l border-t w-1/12 sm:w-auto">
+              OUT
+            </th>
+            <th className="px-4 py-2 border-b border-l border-t w-1/12 sm:w-auto">
+              Delete
+            </th>
+            <th className="px-4 py-2 border-b border-l border-t w-1/12 sm:w-auto">
+              Apply
+            </th>
+          </>
+        )}
+      </tr>
+    </thead>
+    {!filterdProducts
+      ? products?.map((product) => (
+          <TAbleActions
+            key={product._id}
+            product={product}
+            fetchProducts={fetchProducts}
+            setFelterdProducts={setFelterdProducts}
+          />
+        ))
+      : filterdProducts?.map((product) => (
+          <TAbleActions
+            key={product._id}
+            product={product}
+            fetchProducts={fetchProducts}
+            setFelterdProducts={setFelterdProducts}
+          />
+        ))}
+  </table>
+</div>
+
   );
 };
 
