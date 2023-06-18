24b5d7d4483d9f61ecf670b1273cea7c19a576ef
93amrkakashi <93amr.kakashi@gmail.com>
2023-06-17 23:18:07 -0700

    user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,7 +34,7 @@ const signupUser = async (req, res) => {
 
     // create a token
     const token = createToken(user._id);
-
+console.log(user);
     res.status(200).json({ email, token,firstName, lastName , admin, owner});
   } catch (error) {
     res.status(400).json({ error: error.message });
